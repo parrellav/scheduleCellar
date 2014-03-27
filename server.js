4f85ce24b066345bb81aaf30aa82fc4d55547504
Parrella, Vincent <Vincent_Parrella@cable.comcast.com>
2014-03-27 15:42:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    events = require('./routes/events');
+ 
+var app = express();
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+ 
+app.get('/events', events.findAll);
+//app.get('/events/:id', events.findById);
+//app.post('/events', events.addWine);
+//app.put('/events/:id', events.updateWine);
+//app.delete('/events/:id', events.deleteWine);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');

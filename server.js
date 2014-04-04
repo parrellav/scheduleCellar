a2d182badd3bc3da6279e31c850e392b56881739
Parrella, Vincent <Vincent_Parrella@cable.comcast.com>
2014-04-04 13:08:14 -0400

    cross site errors

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:9000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
